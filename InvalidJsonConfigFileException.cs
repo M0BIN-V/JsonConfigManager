a5ad4544ad25f34a081876db28bd02ff01263b3e
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-05-13 19:24:27 +0330

    Add InvalidJsonConfigFileException

---
+++ InvalidJsonConfigFileException.cs
@@ -0,0 +1,16 @@
+﻿namespace JsonConfigManager;
+
+internal class InvalidJsonConfigFileException : Exception
+{
+    public InvalidJsonConfigFileException() : base()
+    {
+    }
+
+    public InvalidJsonConfigFileException(string? message) : base(message)
+    {
+    }
+
+    public InvalidJsonConfigFileException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
